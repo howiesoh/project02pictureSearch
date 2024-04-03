599989b5d2b8647c1ea45c5c7bafb4b6d5a8608e
howie <howie12323@gmail.com>
2024-04-03 14:58:40 +0800

    update build for gh-pages

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />}></Route>
+          <Route path="project02pictureSearch" element={<HomePage />}></Route>
           <Route path="about" element={<About />}></Route>
           <Route path="*" element={<Page404 />}></Route>
         </Route>
